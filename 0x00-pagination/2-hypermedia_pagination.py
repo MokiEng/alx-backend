2a1ea614f3d395a7dd98f1f8cbf47fa21a868c77
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-21 22:34:25 -0700

    Update 2-hypermedia_pagination.py

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""a module  that implements the Server class with the get_page method:
+"""
+import csv
+import math
+from typing import Dict, List, Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of the start and end index for
+    a given page and page_size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initializes a new Server instance.
+        """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a specific page of data from the dataset based on
+        page and page_size.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Get a dictionary containing hypermedia pagination information.
+        """
+        page_data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        hyper_info = {
+            "page_size": len(page_data),
+            "page": page,
+            "data": page_data,
+            "next_page": page + 1 if page < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
+
+        return hyper_info
