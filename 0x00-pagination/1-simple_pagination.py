3cc26157eabbb37559543f85a47286c3405bf8f5
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-21 22:26:22 -0700

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,7 +3,14 @@
 """
 import csv
 from typing import List, Tuple
-from .0-simple_helper_function import index_range
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
